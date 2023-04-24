f88920dba11b88aa0d7c3e0c4d04eab24f9695bb
moolasudhakarreddy31 <moolasudhakarreddy8008@gamil.com>
2023-04-24 21:09:16 +0530

    Added code

--- modelsproject/modelsapp/admin.py
+++ modelsproject/modelsapp/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from modelsapp.models import StudentInfo,TeacharsInfo,ContactInfo1,StudentInfo1,TeacharsInfo1,Parent,SubChild,Child
 
 # Register your models here.
+
+admin.site.register(StudentInfo)
+admin.site.register(TeacharsInfo)
+admin.site.register(ContactInfo1)
+admin.site.register(StudentInfo1)
+admin.site.register(TeacharsInfo1)
+admin.site.register(Parent)
+admin.site.register(SubChild)
+admin.site.register(Child)
+
+
+
+
+
+
--- modelsproject/modelsapp/models.py
+++ modelsproject/modelsapp/models.py
@@ -1,3 +1,66 @@
 from django.db import models
 
 # Create your models here.
+
+# Abstract Type
+class ContactInfo(models.Model):
+    name=models.CharField(max_length=233)
+    email=models.EmailField()
+    address=models.CharField(max_length=433)
+    class Meta:
+        abstract=True
+
+
+class StudentInfo(ContactInfo):
+    rollno=models.IntegerField()
+    marks=models.IntegerField()
+
+class TeacharsInfo(ContactInfo):
+    subject=models.CharField(max_length=233)
+    salary=models.BigIntegerField()
+
+
+
+#Multiple Inhertance Type
+class ContactInfo1(models.Model):
+    name=models.CharField(max_length=233)
+    email=models.EmailField()
+    address=models.CharField(max_length=433)
+
+
+class StudentInfo1(ContactInfo1):
+    rollno=models.IntegerField()
+    marks=models.IntegerField()
+
+class TeacharsInfo1(ContactInfo1):
+    subject=models.CharField(max_length=233)
+    salary=models.BigIntegerField()
+
+
+
+# MultiLevel Inhertance Type
+class Parent(models.Model):
+    fd1=models.CharField(max_length=222)
+    fd2=models.CharField(max_length=222)
+
+class Child(Parent):
+    fd3=models.CharField(max_length=222)
+    fd4=models.CharField(max_length=222)
+
+
+
+class SubChild(Child):
+    fd5=models.CharField(max_length=222)
+
+
+
+
+
+
+
+
+
+
+
+
+
